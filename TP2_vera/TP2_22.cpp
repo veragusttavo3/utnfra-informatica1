14cc5ea584de697fa1ee595c972e55e54250962a
veragusttavo3 <veragusttavo3@gmail.com>
2024-12-06 16:57:37 -0300

    ejercicio 22 ok

--- TP2_vera/TP2_22.cpp
+++ TP2_vera/TP2_22.cpp
@@ -12,19 +12,19 @@ e)	Salir del programa.
 #include<ctype.h>
 #include<math.h>
 //constantes globales
-#define alumnos 30
+#define alumnos 3
 #define caracteres 50
 //variables globales
 char opcion='q';
 int aproDirecta=0;
 int darFinal=0;
 int recursan=0;
-int promAlum[alumnos]={2,3,4,2,4,5,6,5,5,4,6,7,5,4,3,4,6,7,8,7,6,8,6,5,4,3,6,7,8,9};
-int legajoAlum[alumnos]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
-char nombreAlum[alumnos][caracteres]={"Lucas","Maria"};
+int promAlum[alumnos]={0};
+int legajoAlum[alumnos]={0};
+char nombreAlum[alumnos][caracteres];
 //funciones  m5
 void imprimirDetalles(){
-    for(int i=0;i<strlen(aproDirecta);i++){
+    for(int i=0;i<sizeof(aproDirecta)/4;i++){
         printf("Aprobacion directa: %i alumnos\n",aproDirecta);
         printf("Dar final: %i alumnos\n",darFinal);
         printf("Resursan: %i alumnos\n",recursan);
@@ -35,31 +35,45 @@ void recursantes(){
     for(int i=0;i<alumnos;i++){
     if(promAlum[i]<4){
         //recursan
-        printf("N° de Legajo: %i nombre: %s promedio: %i\n",legajoAlum[i],nombreAlum[i],promAlum[i]);
+        printf("Num de Legajo: %i nombre: %s promedio: %i\n",legajoAlum[i],nombreAlum[i],promAlum[i]);
         recursan++;
     }}
 }
 void promedioEntre6y3(){
-    
     for(int i=0;i<alumnos;i++){
     if(promAlum[i]<6 && promAlum[i]>=4){
         //examen
-        printf("N° de Legajo: %i nombre: %s promedio: %i\n",legajoAlum[i],nombreAlum[i],promAlum[i]);
+        printf("Num de Legajo: %i nombre: %s promedio: %i\n",legajoAlum[i],nombreAlum[i],promAlum[i]);
         darFinal++;
     }}
 }
 void promedioMayorA6(){
 
     for(int i=0;i<alumnos;i++){
-    if(promAlum[i]>6){
+    if(promAlum[i]>=6){
         //aprobaron
-        printf("N° de Legajo: %i nombre: %s promedio: %i\n",legajoAlum[i],nombreAlum[i],promAlum[i]);
+        printf("Num de Legajo: %i nombre: %s promedio: %i\n",legajoAlum[i],nombreAlum[i],promAlum[i]);
         aproDirecta++;
     }}
 }
+void cargarAlumnos(){
+    printf("Cargar alumnos;\n");
+    for(int i=0;i<alumnos;i++){
+        //n° de legajo
+    printf("Numero de legajo: ");
+    scanf("%i",&legajoAlum[i]);
+    //nombre
+    printf("Nombre: ");
+    scanf("%s",nombreAlum[i]);
+    //promedio
+    printf("Promedio: ");
+    scanf("%i",&promAlum[i]);
+    }
+}
 void menu(){
         switch(opcion){
-                case 'a':   promedioMayorA6();
+                case 'a':   cargarAlumnos();
+                            promedioMayorA6();
                 break;
                 case 'b':   promedioEntre6y3();
                 break;
@@ -71,19 +85,21 @@ void menu(){
                 break;
                 default:    printf("Elije de nuevo");
                 break;
-        }}
+        }
+        getchar(); // Limpia el búfer
+        }
 void introducirOpcion(){
-        fflush(stdin);
         scanf("%c",&opcion);
         tolower(opcion);
-        getchar(); // Limpia el búfer
+        
         }
 void mensajeMenu(){
         printf("Menu:\n"
-        "A- Lograron la aprobación directa (promedio mayor o igual a 6)\n"
+        "A- Lograron la aprobacion directa (promedio mayor o igual a 6)\n"
         "B- Deben dar el examen final (promedio menor a 6 y mayor o igual a 4)\n"
-        "C- Indicar cuántos alumnos obtuvieron la aprobación directa, dan final o recursan.\n"
-        "D- Salir del programa\n");}
+        "c- Deben recursar la materia (promedio menor a 4).\n"
+        "D- Indicar cuantos alumnos obtuvieron la aprobacion directa, dan final o recursan.\n"
+        "E- Salir del programa\n");}
 //inicio del programa
 int main(){
     //cargar datos del alumno para probar
@@ -91,6 +107,6 @@ int main(){
         mensajeMenu();
         introducirOpcion();
         menu();
-        }while(opcion!='d');
+        }while(opcion!='e');
 return 0;
 }
